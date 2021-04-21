1e44df01bcb52655631a53f7e2103d7864ec9322
Sally Hoffmann <shoffmann2791@gmail.com>
2021-04-20 20:42:08 -0500

    js 2.2.3

---
+++ ClassActivites/2.2.3 .js
@@ -0,0 +1,39 @@
+// create a variable "timeOfDay" and assign it a string
+
+let timeOfDay = "evening"
+// create a variable "greeting" that references a template literal
+let greeting = `I hope you are having a wonderful ${timeOfDay}!!!!!`
+// use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+console.log(greeting);
+
+// print "greeting"
+
+
+// create a new variable, but do not assign it a value
+let savvycoders;
+
+// print the variable and its type
+
+console.log(savvycoders);
+console.log(typeof savvycoders);
+// ? what type should we expect?
+
+
+// assign the variable a value that indicates the variable is purposely blank
+// ? what value should we assign?
+savvycoders = null;
+
+// print the variable and its type again
+// ? what type should we expect?
+console.log(savvycoders);
+console.log(typeof savvycoders);
+// try to print a variable that does not exist
+// ? what should we expect to print in the CLI?
+
+//console.log(javascript);
+
+// print "greeting" again
+// ? will this line run?
+
+
+
