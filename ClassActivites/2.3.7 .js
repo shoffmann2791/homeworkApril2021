f56f3d4f026ad876106c870f7a7613caf0a8c027
Sally Hoffmann <shoffmann2791@gmail.com>
2021-04-21 21:18:40 -0500

    math op

---
+++ ClassActivites/2.3.7 .js
@@ -0,0 +1,14 @@
+// ? TO DO: create a while loop that TOTALS the numbers from 1 to 100
+// create a variable to represent the current number
+let CurNumber;
+curNumber = 1;
+
+// create a variable to represent the current total
+let curTotal = 0;
+
+// write a while loop that sums the numbers from 1 to 100 
+while (curNumber <= 100) {
+curTotal += CurNumber;
+curNumber++;
+};
+console.log(curTotal)
