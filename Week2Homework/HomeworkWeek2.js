8a2166121d05c5d728004b6a9763f8bab44f83fb
Sally Hoffmann <shoffmann2791@gmail.com>
2021-04-23 19:38:38 -0500

    week2

---
+++ Week2Homework/HomeworkWeek2.js
@@ -0,0 +1,14 @@
+let pizzaPlace;
+let numberOfToppings;
+
+pizzaPlace = "Pizza Pie";
+
+numberOfToppings = 5;
+
+console.log(`The name of my pizza shop is ${pizzaPlace}. We have ${numberOfToppings} toppings to choose from!`);
+
+  if (numberOfToppings <=10) { 
+    console.log("Quality, not quantity.");
+  } else {
+    console.log("A whole lot of pizza.");
+  }
