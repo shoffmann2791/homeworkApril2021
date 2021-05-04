f627df61f7b3494585dedc645571c5c98545bbc0
Sally Hoffmann <shoffmann2791@gmail.com>
2021-05-03 20:15:21 -0500

    4.1.2

---
+++ ClassActivites/4.1.2.js
@@ -0,0 +1,9 @@
+// select the root node
+document.querySelector("p").getRootNode();
+// select the last child of the root node
+document.querySelector("html").lastElementChild;
+// select all the children of the body element
+document.getElementsByTagName("body").children;
+// select the next sibling of the h2 node
+document.querySelector("h2").nextElementSibling;
+// select the parent element of the h1 node
